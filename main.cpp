a3770ff452eaa2eadaa6b87ede5a7d3400f8b31e
Matheus Vagner <>
2021-05-22 01:22:16 +0000

    Adicionado um loop para o código poder rodar várias vezes

--- main.cpp
+++ main.cpp
@@ -18,38 +18,44 @@ int transformador(char *palavra, char *palavra_codificada, char letra, char nova
 
 int main() {
   string mensagem, nova_mensagem; 
-  int trocas, tamanho;
+  int trocas, tamanho, controlador = 1;
   char letra, nova_letra, *ponteiro= NULL, *ponteiro_novo = NULL;
 
-  cout << "Digite a mensagem que quer trocar seus caracteres, o caracter a ser substituido, e o caracter substituto" << endl;
-  cout << "INPUT:" << endl;
-  cin >> mensagem;
-  cin >> letra;
-  cin >> nova_letra;
+  while (controlador == 1){
+    cout << "Digite a mensagem que quer trocar seus caracteres, o caracter a ser substituido, e o caracter substituto" << endl;
+    cout << "INPUT:" << endl;
+    cin >> mensagem;
+    cin >> letra;
+    cin >> nova_letra;
 
-  tamanho = mensagem.length();
+    tamanho = mensagem.length();
 
-  char  vet_char[tamanho], vet_novo[tamanho];
+    char  vet_char[tamanho], vet_novo[tamanho];
 
-  for (int i = 0; i < tamanho; i++){
-    vet_char[i] = mensagem[i];
+    for (int i = 0; i < tamanho; i++){
+      vet_char[i] = mensagem[i];
+    }
+
+    cout << endl;
+    
+    ponteiro = &vet_char[0];
+    ponteiro_novo = &vet_novo[0];
+
+    trocas = transformador(ponteiro, ponteiro_novo, letra, nova_letra, tamanho);
+
+    cout << "OUTPUT:" << endl;
+
+    cout << mensagem << endl;
+
+    for (int i = 0; i < tamanho; i++){
+      cout << vet_novo[i];
+    }
+    
+    cout << endl << trocas << endl;
+
+    cout << "Deseja continuar? 1 para sim, 0 para não" <<endl;
+    cin >> controlador;
+
   }
-
-  cout << endl;
-  
-  ponteiro = &vet_char[0];
-  ponteiro_novo = &vet_novo[0];
-
-  trocas = transformador(ponteiro, ponteiro_novo, letra, nova_letra, tamanho);
-
-  cout << "OUTPUT:" << endl;
-
-  cout << mensagem << endl;
-
-  for (int i = 0; i < tamanho; i++){
-    cout << vet_novo[i];
-  }
-  
-  cout << endl << trocas << endl;
   return 0;
 }
